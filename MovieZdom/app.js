6101d9b2dd4a3ed1f920f3494fe5948bb5efdff5
akismgs <xarhsvrahas@gmail.com>
2026-01-08 20:27:52 +0200

    adds

--- MovieZdom/app.js
+++ MovieZdom/app.js
@@ -344,6 +344,4 @@ function reveal(lobbyId, index) {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`🚀 Server: http://localhost:${PORT}`));
